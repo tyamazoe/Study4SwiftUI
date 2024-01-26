8f424eb68afd67d43608df223a6c04ad206bb0cd
Tomohisa Yamazoe <tomohisa.yamazoe@gmail.com>
2024-01-26 20:10:22 +0900

    Initial Commit

---
+++ Study4SwiftUI/Study4SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Study4SwiftUIApp.swift
+//  Study4SwiftUI
+//
+//  Created by Tomohisa Yamazoe on 2024/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Study4SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
