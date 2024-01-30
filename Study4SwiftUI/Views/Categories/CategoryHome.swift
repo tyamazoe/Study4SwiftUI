53ed2ad80788fef189385752515911b8417dc571
Tomohisa Yamazoe <tomohisa.yamazoe@gmail.com>
2024-01-30 10:34:27 +0900

    3-1 Interfacing with UIKit

--- Study4SwiftUI/Views/Categories/CategoryHome.swift
+++ Study4SwiftUI/Views/Categories/CategoryHome.swift
@@ -14,11 +14,13 @@ struct CategoryHome: View {
     var body: some View {
         NavigationSplitView {
             List {
-                modelData.features[0].image
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 200)
-                    .clipped()
+//                modelData.features[0].image
+//                    .resizable()
+//                    .scaledToFill()
+//                    .frame(height: 200)
+//                    .clipped()
+//                    .listRowInsets(EdgeInsets())
+                PageView(pages: modelData.features.map { FeatureCard(landmark: $0) })
                     .listRowInsets(EdgeInsets())
 
                 ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
